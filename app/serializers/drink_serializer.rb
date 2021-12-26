62b53cf33a7d3ae55877dfcb111fb046c8212725
Omar <omar.branez@gmail.com>
2021-12-26 15:44:32 -0500

    serialize relationship between drinks and products

--- app/serializers/drink_serializer.rb
+++ app/serializers/drink_serializer.rb
@@ -1,6 +1,8 @@
 class DrinkSerializer
   include JSONAPI::Serializer
   belongs_to :user
-  has_many :products
   attributes :id, :name, :drink_type, :glass_type, :instructions, :photo_url
+  attribute :products do |drink|
+    drink.products
+  end
 end
--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,11 @@
 class ProductSerializer
   include JSONAPI::Serializer
   set_key_transform :camel_lower
+
   attributes :id, :name, :category, :subcategory, :description
+
+  attribute :drinks do |product|
+    # binding.pry
+    product.drinks
+  end
 end
