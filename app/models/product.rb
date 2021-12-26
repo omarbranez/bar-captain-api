4fa1b33c6e9c20dcdd55a1d21d51419d7df9b8c4
Omar <omar.branez@gmail.com>
2021-12-26 15:19:37 -0500

    fix issue with drinks_products join table

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
     has_many :products_users
     has_many :users, through: :products_users
-    has_many :drink_products
-    has_many :drinks, through: :drink_products
+    has_many :drinks_products
+    has_many :drinks, through: :drinks_products
 end
