615a68d0c1379b06d06f7de340845feaefa0c0eb
Omar <omar.branez@gmail.com>
2021-12-28 17:32:30 -0500

    validate ability to make drinks after adding a product

--- app/models/products_user.rb
+++ app/models/products_user.rb
@@ -1,4 +1,18 @@
 class ProductsUser < ApplicationRecord
     belongs_to :user
     belongs_to :product
+
+    after_create :update_drinks_user
+
+    def possible_drinks
+        user = User.find(ProductsUser.last.user_id)
+        Drink.all.select{|drink|(drink.products.ids - user.product_ids).empty?}
+    end
+
+    def update_drinks_user
+        user = User.find(ProductsUser.last.user_id)
+        possible_drinks.each do |drink|
+            DrinksUser.find_or_create_by(user_id: user.id, drink_id: drink.id)
+        end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ApplicationRecord
     has_secure_password
+    validates_presence_of :name
+    validates_presence_of :password
+
     has_many :drinks_users
     has_many :drinks, through: :drinks_users
     has_many :products_users
     has_many :products, through: :products_users
+
+    def self.possible_drinks
+        user = User.find(ProductsUser.last.user_id)
+        Drink.all.select{|drink|(drink.products.ids - user.product_ids).empty?}.each do |drink|
+            DrinksUser.find_or_create_by(user_id: user.id, drink_id: drink.id)
+        end
+    end
 end
