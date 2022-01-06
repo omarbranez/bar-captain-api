905ee7fe6bd1edb3e4afd7ed1e0de97754301a30
Omar <omar.branez@gmail.com>
2022-01-05 23:22:14 -0500

    render snackbar with message on successful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,9 @@ class SessionsController < ApplicationController
     def create
       user = User.find_by_username(params[:username])
       if user && user.authenticate(params[:password])
-        render_user_with_token(user)
+        # render_user_with_token(user)
+        token = encode_token(user.id)
+        render json: {user: UserSerializer.new(user), token: token, variant: "success", message: "Successfully Logged In As #{user.username}"}
       else
         render json: {errors: "Login Attempt Unsuccessful"}, status: :forbidden
       end
